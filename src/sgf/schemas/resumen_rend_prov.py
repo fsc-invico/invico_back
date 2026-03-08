a57793fb617864a84bd39576c3e0041d712026d4
Fernando Corrales <fscorrales@gmail.com>
2026-03-08 19:45:16 -0300

    update SGF resumen_rend_prov's schemas

--- src/sgf/schemas/resumen_rend_prov.py
+++ src/sgf/schemas/resumen_rend_prov.py
@@ -1,46 +1,23 @@
 __all__ = [
-    "ResumenRendProvParams",
     "ResumenRendProvReport",
     "ResumenRendProvDocument",
-    "ResumenRendProvFilter",
+    "ResumenRendProvFullFilter",
+    "ResumenRendProvLiteFilter",
 ]
 
 
-from datetime import date, datetime
-from typing import List, Optional
+from datetime import datetime
+from typing import Optional
 
 from pydantic import (
     BaseModel,
     Field,
     NonNegativeFloat,
-    field_validator,
-    model_validator,
 )
 from pydantic_mongo import PydanticObjectId
 
-from ...utils import BaseFilterParams, CamelModel, ErrorsWithDocId
+from ...utils import BaseFilterParams, CamelModel
 from .common import Origen
-
-
-# --------------------------------------------------
-class ResumenRendProvParams(CamelModel):
-    ejercicio_desde: int = Field(default=date.today().year)
-    ejercicio_hasta: int = Field(default=date.today().year)
-    origen: Optional[Origen] = None
-
-    @field_validator("ejercicio_desde", "ejercicio_hasta")
-    @classmethod
-    def validate_ejercicio_range(cls, v: int) -> int:
-        current_year = date.today().year
-        if not (2010 <= v <= current_year):
-            raise ValueError(f"El ejercicio debe estar entre 2010 y {current_year}")
-        return v
-
-    @model_validator(mode="after")
-    def check_range(self) -> "ResumenRendProvParams":
-        if self.ejercicio_hasta < self.ejercicio_desde:
-            raise ValueError("Ejercicio Desde no puede ser menor que Ejercicio Hasta")
-        return self
 
 
 # -------------------------------------------------
@@ -73,15 +50,20 @@ class ResumenRendProvDocument(ResumenRendProvReport):
     id: PydanticObjectId = Field(alias="_id")
 
 
+# Este se usa para la tabla (UI)
 # -------------------------------------------------
-class ResumenRendProvFilter(BaseFilterParams):
+class ResumenRendProvFullFilter(BaseFilterParams):
     origen: Optional[Origen] = None
     ejercicio: Optional[int] = None
     beneficiario: Optional[str] = None
     cta_cte: Optional[str] = None
 
 
+# Este se usa para el Excel y Borrar (Sin limit/offset)
 # -------------------------------------------------
-class ResumenRendProvValidationOutput(BaseModel):
-    errors: List[ErrorsWithDocId]
-    validated: List[ResumenRendProvDocument]
+class ResumenRendProvLiteFilter(CamelModel):
+    query_filter: str = ""
+    origen: Optional[Origen] = None
+    ejercicio: Optional[int] = None
+    beneficiario: Optional[str] = None
+    cta_cte: Optional[str] = None
