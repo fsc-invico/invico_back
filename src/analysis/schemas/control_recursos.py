c32fb228d23881a97964a1993163c5bd205fa5b4
Fernando Corrales <fscorrales@gmail.com>
2026-03-08 20:08:12 -0300

    update control_recursos's schemas

--- src/analysis/schemas/control_recursos.py
+++ src/analysis/schemas/control_recursos.py
@@ -1,53 +1,17 @@
 __all__ = [
-    "ControlRecursosParams",
-    "ControlRecursosSyncParams",
     "ControlRecursosReport",
     "ControlRecursosDocument",
-    "ControlRecursosFilter",
+    "ControlRecursosFullFilter",
+    "ControlRecursosLiteFilter",
 ]
 
-import os
-from datetime import date
+
 from typing import Optional
 
-from pydantic import BaseModel, Field, field_validator, model_validator
+from pydantic import BaseModel, Field
 from pydantic_mongo import PydanticObjectId
 
-from ...utils import BaseFilterParams, CamelModel, get_sscc_cta_cte_path
-
-
-# --------------------------------------------------
-class ControlRecursosParams(CamelModel):
-    ejercicio_desde: int = Field(default=date.today().year)
-    ejercicio_hasta: int = Field(default=date.today().year)
-    # ejercicio_from: int = date.today().year
-    # ejercicio_to: int = date.today().year
-
-    @field_validator("ejercicio_desde", "ejercicio_hasta")
-    @classmethod
-    def validate_ejercicio_range(cls, v: int) -> int:
-        current_year = date.today().year
-        if not (2010 <= v <= current_year):
-            raise ValueError(f"El ejercicio debe estar entre 2010 y {current_year}")
-        return v
-
-    @model_validator(mode="after")
-    def check_range(self) -> "ControlRecursosParams":
-        if self.ejercicio_hasta < self.ejercicio_desde:
-            raise ValueError("Ejercicio Desde no puede ser menor que Ejercicio Hasta")
-        return self
-
-
-# --------------------------------------------------
-class ControlRecursosSyncParams(ControlRecursosParams):
-    siif_username: Optional[str] = None
-    siif_password: Optional[str] = None
-    sscc_username: Optional[str] = None
-    sscc_password: Optional[str] = None
-    ctas_ctes_excel_path: Optional[str] = Field(
-        default=os.path.join(get_sscc_cta_cte_path(), "cta_cte.xlsx"),
-        description="Ruta al archivo Ctas Ctes EXCEL",
-    )
+from ...utils import BaseFilterParams, CamelModel
 
 
 # -------------------------------------------------
@@ -65,7 +29,17 @@ class ControlRecursosDocument(ControlRecursosReport):
     id: PydanticObjectId = Field(alias="_id")
 
 
+# Este se usa para el Excel y Borrar (Sin limit/offset)
 # -------------------------------------------------
-class ControlRecursosFilter(BaseFilterParams):
+class ControlRecursosFullFilter(BaseFilterParams):
     ejercicio: Optional[int] = None
     fuente: Optional[int] = None
+
+
+# Este se usa para el Excel y Borrar (Sin limit/offset)
+# -------------------------------------------------
+class ControlRecursosLiteFilter(CamelModel):
+    query_filter: str = ""
+    ejercicio: Optional[int] = None
+    fuente: Optional[int] = None
+    # Aquí podrías añadir: incluir_detalles: bool = False
