202a5641a820acaa8e109ebfdf3a4d02e227d821
Fernando Corrales <fscorrales@gmail.com>
2026-03-12 19:17:03 -0300

    add .python-version, Procfile and update run.py for Koyeb deployment

--- run.py
+++ run.py
@@ -2,9 +2,8 @@ import uvicorn
 
 if __name__ == "__main__":  # 🔹 Importante para evitar problemas en Windows
     uvicorn.run(
-        # "src.main:app", host="0.0.0.0", port=8000, reload=False, loop="asyncio"
         "src.main:app",
-        host="127.0.0.1",
+        host="0.0.0.0",  # 127.0.0.1
         port=8000,
         reload=False,
         loop="asyncio",
